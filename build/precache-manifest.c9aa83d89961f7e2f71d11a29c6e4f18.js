2c456198b17baa5c4fa8cb528933ed035cab3b8f
Lester016 <37885860+Lester016@users.noreply.github.com>
2018-11-19 15:27:51 -0800

    Add Files via Upload
    Build file of my project called Landian

---
+++ build/precache-manifest.c9aa83d89961f7e2f71d11a29c6e4f18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c42d6506a1a3bc12c1bc490c85784da9",
+    "url": "/static/media/sampleMockUp.c42d6506.png"
+  },
+  {
+    "revision": "7ef4555102b52b9993d27c7c9515d613",
+    "url": "/static/media/ios-badge.7ef45551.png"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4a60378c0792aaa1c3c2",
+    "url": "/static/js/main.4a60378c.chunk.js"
+  },
+  {
+    "revision": "e94a8524328b6f8efa74",
+    "url": "/static/js/1.e94a8524.chunk.js"
+  },
+  {
+    "revision": "4a60378c0792aaa1c3c2",
+    "url": "/static/css/main.47665ed7.chunk.css"
+  },
+  {
+    "revision": "4153833053ffd303664f6918e1076216",
+    "url": "/index.html"
+  }
+];
